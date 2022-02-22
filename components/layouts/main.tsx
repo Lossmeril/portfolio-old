6c4e294a1dbb64efac9732dcc2b795254aa2dfa7
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2022-02-22 13:33:16 +0000

    3D Model Implementation

--- components/layouts/main.tsx
+++ components/layouts/main.tsx
@@ -2,6 +2,14 @@ import Head from "next/head";
 import Navbar from "../navbar"
 import {Box, Container} from "@chakra-ui/react"
 
+import ModelLoader from '../3d-model-loader'
+import dynamic from "next/dynamic";
+
+const LazyModel = dynamic(() => import('../3d-model'), {
+  ssr: false,
+  loading: () => <ModelLoader />
+})
+
 const Main = ({children, router}) => {
     return (
         <Box as="main" pb={8}>
@@ -12,7 +20,8 @@ const Main = ({children, router}) => {
 
             <Navbar path={router.asPath} />
 
-            <Container maxW="container.md" pt={"140"}>
+            <Container maxW="container.md" pt={140}>
+                <LazyModel />
                 {children}
             </Container>
         </Box>
