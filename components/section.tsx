45ca09d560b178e370d95b6e11f3d0bd6c218383
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2022-02-23 20:12:33 +0000

    Removed unused import

--- components/section.tsx
+++ components/section.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion"
-import { chakra, shouldForwardProp, transform } from "@chakra-ui/react"
+import { chakra, shouldForwardProp, } from "@chakra-ui/react"
 
 const StyledDiv = chakra(motion.div, {
     shouldForwardProp: prop => {
